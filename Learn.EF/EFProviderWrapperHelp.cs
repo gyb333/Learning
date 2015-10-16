931bab38af5719bb55d458f270186449e4e9f8af
gyb333 <876368840@qq.com>
2015-10-16 17:53:14 +0800

    2015年10月16日 17:53:09

---
+++ Learn.EF/EFProviderWrapperHelp.cs
@@ -0,0 +1,63 @@
+﻿using EFCachingProvider;
+using EFCachingProvider.Caching;
+using EFTracingProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.EF
+{
+    public class EFProviderWrapperHelp
+    {
+        public void Init()
+        {
+            EFTracingProviderConfiguration.RegisterProvider();
+            EFCachingProviderConfiguration.RegisterProvider();
+#if LOGGING
+            EFTracingProviderConfiguration.LogToConsole = true; //Optional: for sending tracing to console
+#endif
+
+        }
+
+
+        private static void SimpleCachingDemo()
+        {
+            ICache cache = new InMemoryCache();
+            CachingPolicy cachingPolicy = CachingPolicy.CacheAll;
+
+            // log SQL from all connections to the console
+            EFTracingProviderConfiguration.LogToConsole = true;
+
+            for (int i = 0; i < 3; ++i)
+            {
+                Console.WriteLine();
+                Console.WriteLine("*** Pass #{0}...", i);
+                Console.WriteLine();
+                using (var context = new ExtendedNorthwindEntities())
+                {
+                    // set up caching
+                    context.Cache = cache;
+                    context.CachingPolicy = cachingPolicy;
+
+                    Console.WriteLine("Loading customer...");
+                    var cust = context.Customers.First(c => c.CustomerID == "ALFKI");
+                    Console.WriteLine("Customer name: {0}", cust.ContactName);
+                    Console.WriteLine("Loading orders...");
+                    cust.Orders.Load();
+                    Console.WriteLine("Order count: {0}", cust.Orders.Count);
+                }
+            }
+
+            Console.WriteLine();
+
+            //Console.WriteLine("*** Cache statistics: Hits:{0} Misses:{1} Hit ratio:{2}% Adds:{3} Invalidations:{4}",
+            //    cache.CacheHits,
+            //    cache.CacheMisses,
+            //    100.0 * cache.CacheHits / (cache.CacheHits + cache.CacheMisses),
+            //    cache.CacheItemAdds,
+            //    cache.CacheItemInvalidations);
+        }
+    }
+}
---
+++ Learn.EF/ExtendedDataBase.cs
@@ -0,0 +1,140 @@
+﻿using EFCachingProvider;
+using EFCachingProvider.Caching;
+using EFProviderWrapperToolkit;
+using EFTracingProvider;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFTracingProvider;
+
+namespace Learn.EF
+{
+    public class ExtendedDataBase:DbContext
+    {
+        private TextWriter logOutput;
+        //private static ILog logger;
+        public ExtendedDataBase()
+
+        {
+            //// 初始化Log4net,配置在独立的"log4net.config"中配置
+            //log4net.Config.XmlConfigurator.Configure(new FileInfo("log4net.config"));
+            //// 初始化一个logger
+            //logger = log4net.LogManager.GetLogger("EFLog4net");
+
+           
+            
+            // 输出 TraceString (SQL文)
+            //logger.Debug(Environment.NewLine + e.ToTraceString().TrimEnd());
+ 
+        }
+        public ExtendedDataBase(string connectionString)
+            : base(EntityConnectionWrapperUtils.CreateEntityConnectionWithWrappers(
+                    connectionString,
+                    "EFTracingProvider",
+                    "EFCachingProvider"
+            ),false)
+        {
+
+        }
+
+
+
+        #region Tracing Extensions
+
+        private EFTracingConnection TracingConnection
+        {
+            get
+            {
+                IObjectContextAdapter  oca = this as IObjectContextAdapter;
+                return oca.ObjectContext.Connection.UnwrapConnection<EFTracingConnection>();
+  
+            }
+        }
+
+        public event EventHandler<CommandExecutionEventArgs> CommandExecuting
+        {
+            add { this.TracingConnection.CommandExecuting += value; }
+            remove { this.TracingConnection.CommandExecuting -= value; }
+        }
+
+        public event EventHandler<CommandExecutionEventArgs> CommandFinished
+        {
+            add { this.TracingConnection.CommandFinished += value; }
+            remove { this.TracingConnection.CommandFinished -= value; }
+        }
+
+        public event EventHandler<CommandExecutionEventArgs> CommandFailed
+        {
+            add { this.TracingConnection.CommandFailed += value; }
+            remove { this.TracingConnection.CommandFailed -= value; }
+        }
+
+        private void AppendToLog(object sender, CommandExecutionEventArgs e)
+        {
+            if (this.logOutput != null)
+            {
+                this.logOutput.WriteLine(e.ToTraceString().TrimEnd());
+                this.logOutput.WriteLine();
+            }
+        }
+
+        public TextWriter Log
+        {
+            get { return this.logOutput; }
+            set
+            {
+                if ((this.logOutput != null) != (value != null))
+                {
+                    if (value == null)
+                    {
+                        CommandExecuting -= AppendToLog;
+                    }
+                    else
+                    {
+                        CommandExecuting += AppendToLog;
+                    }
+                }
+
+                this.logOutput = value;
+            }
+        }
+
+
+        #endregion
+
+        #region Caching Extensions
+
+        private EFCachingConnection CachingConnection
+        {
+            get
+            {
+                IObjectContextAdapter oca = this as IObjectContextAdapter;
+                return oca.ObjectContext.Connection.UnwrapConnection<EFCachingConnection>();
+            }
+        }
+
+        public ICache Cache
+        {
+            get { return CachingConnection.Cache; }
+            set { CachingConnection.Cache = value; }
+        }
+
+        public CachingPolicy CachingPolicy
+        {
+            get { return CachingConnection.CachingPolicy; }
+            set { CachingConnection.CachingPolicy = value; }
+        }
+
+        #endregion
+
+
+    }
+}
