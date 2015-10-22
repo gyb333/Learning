07056275c2790d7c18766598db8af302c1998082
gyb333 <876368840@qq.com>
2015-10-22 18:11:49 +0800

    2015年10月22日 18:11:41

---
+++ Learn.CacheCow/CacheCowHelper.cs
@@ -0,0 +1,33 @@
+﻿using CacheCow.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Learn.CacheCow
+{
+    class CacheCowHelper
+    {
+        void init(HttpConfiguration config)
+        {
+            var cacheCowCacheHandler = new CachingHandler(config);
+            config.MessageHandlers.Add(cacheCowCacheHandler);
+
+
+            //客户端
+
+
+            HttpClient client = new HttpClient(new WebRequestHandler()
+            {
+                CachePolicy = new RequestCachePolicy(RequestCacheLevel.Default)
+            });
+
+            //var httpResponseMessage = await client.GetAsync("http://superpoopy");
+
+        }
+    }
+}
